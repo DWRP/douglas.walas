9de4bd3b7fa854695dbcc08b51dde4cd14d0debd
Douglas Walas Rodrigues Pardim <douglas.skd@gmail.com>
2020-09-03 02:05:18 +0000

    testing build

---
+++ precache-manifest.bab1c25ca77d540a85f0ad7c60611996.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85f8cc9537ea07d7c285ce15f9c191da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7016322b388c8a94856c",
+    "url": "/static/css/main.dda68731.chunk.css"
+  },
+  {
+    "revision": "d81bf70d219a1674590b",
+    "url": "/static/js/2.57b396eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.57b396eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7016322b388c8a94856c",
+    "url": "/static/js/main.66994135.chunk.js"
+  },
+  {
+    "revision": "d0a598c2c4f910599d51",
+    "url": "/static/js/runtime-main.e7310a31.js"
+  },
+  {
+    "revision": "e257bfd2339a95ef2dcb7b3cdcb3b6ee",
+    "url": "/static/media/app-ideas.e257bfd2.svg"
+  },
+  {
+    "revision": "f158f8355efd653ca98f7c5b6269a12e",
+    "url": "/static/media/background.f158f835.svg"
+  },
+  {
+    "revision": "e4aaada2f77cc3a5cb155863f728790f",
+    "url": "/static/media/logo.e4aaada2.svg"
+  },
+  {
+    "revision": "b0a62529208e739b4bdfaea341109ca1",
+    "url": "/static/media/logo_dark.b0a62529.svg"
+  },
+  {
+    "revision": "a29e446217eef43cd85dbae0d4d64b59",
+    "url": "/static/media/off_button.a29e4462.svg"
+  },
+  {
+    "revision": "04582740ae30698a8e1d1f09e9f5c084",
+    "url": "/static/media/on_button.04582740.svg"
+  },
+  {
+    "revision": "44f9fd7ed113246aab58d2452ef31c62",
+    "url": "/static/media/profile.44f9fd7e.jpg"
+  }
+]);
