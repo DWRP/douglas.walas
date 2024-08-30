ca7b52c0daf2bf4a1a5ff835f4c750607b52e219
Douglas Pardim <douglas.pardim@globalsys.com.br>
2024-08-29 21:10:02 -0300

    fix: added more SEO items

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -8,10 +8,33 @@ export const metadata: Metadata = {
   title: "Douglas Pardim",
   description:
     "Bringing frontend and backend together to create fluid and scalable web applications!",
+  metadataBase: new URL("https://dwrp.github.io/"),
+  keywords: [
+    "Douglas Pardim",
+    "Douglas Pardim Fullstack",
+    "Douglas Pardim Developer",
+    "Douglas Pardim Fulstack Developer",
+    "Fullstack Developer",
+    "React Developer",
+    "Node.js Developer",
+    "React Native Developer",
+    "JavaScript Expert",
+    "Web Development",
+    "Mobile Development",
+    "UI/UX Development",
+    "E-commerce Development",
+    "React Developer (5+ years)",
+    "Node.js Developer (5+ years)",
+  ],
+  openGraph: {
+    description:
+      "Full-stack developer proficient in React, Node.js, and React Native. I create intuitive interfaces using Tailwind CSS and TypeScript. Expert in Next.js and Nest.js for scalable applications.",
+    images: ["https://dwrp.github.io/cover", "https://dwrp.github.io/cover2"],
+  },
 };
 
 export function generateStaticParams() {
-  return locales.map((locale) => ({locale}));
+  return locales.map((locale) => ({ locale }));
 }
 
 export default async function RootLayout({
